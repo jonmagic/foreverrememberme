6bca6677785310aa97f7588b33a7d8e26a741663
jonmagic <jonmagic@9ed7c148-c707-0410-ad76-f59c66615308>
2006-02-11 20:41:38 +0000

    memorial expiration logic added
    git-svn-id: http://store.sabretechllc.com/public/4ever/trunk@71 9ed7c148-c707-0410-ad76-f59c66615308

--- app/helpers/memorial_helper.rb
+++ app/helpers/memorial_helper.rb
@@ -1,9 +1,14 @@
 module MemorialHelper
+  def extend_memorial_link(memorial)
+    '<a href="#">extend this memorial for 10 years</a>'
+  end
+  
+  
   def show_memorial_image(memorial, width=300)    
     source = if memorial.image.blank? 
               '/images/noimage.jpg'
              else
-               memorial.image
+               memorial.image
              end
     image_tag(source, :width => width, :class => "picture-memorial")
   end
