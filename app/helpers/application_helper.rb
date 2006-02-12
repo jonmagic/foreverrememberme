9876b288367734a86c83a652cc01669ffcb8a484
jonmagic <jonmagic@9ed7c148-c707-0410-ad76-f59c66615308>
2006-02-12 21:47:13 +0000

    warning support added to flash
    git-svn-id: http://store.sabretechllc.com/public/4ever/trunk@105 9ed7c148-c707-0410-ad76-f59c66615308

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include SystemSettingsHelper
+
+  def flash_div *keys
+    keys.collect { |key| content_tag(:div, flash[key],
+                                     :class => "flash #{key}") if flash[key] }.join
+  end
 end
