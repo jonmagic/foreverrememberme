926d875861c5ce0ebadb67df947693da3d3b596b
jonmagic <jonmagic@9ed7c148-c707-0410-ad76-f59c66615308>
2006-03-29 21:02:31 +0000

    integrated lightbox
    git-svn-id: http://store.sabretechllc.com/public/4ever/trunk@172 9ed7c148-c707-0410-ad76-f59c66615308

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,6 @@ class Picture < ActiveRecord::Base
   validates_presence_of [ :image ]
   belongs_to :memorial
   file_column :image, :magick => {
-      :versions => { "thumb" => "80x60", "medium" => "320x240", "large" => "800x600" }
+      :versions => { "thumb" => "80x60", "medium" => "320x240", "large" => "640x480" }
     }
 end
