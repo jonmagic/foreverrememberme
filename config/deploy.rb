7bd4ec61af2c011eefe7e278b57bfbc9d9913155
jonmagic <jonmagic@9ed7c148-c707-0410-ad76-f59c66615308>
2007-05-08 03:09:42 +0000

    just because
    git-svn-id: http://store.sabretechllc.com/public/4ever/trunk@180 9ed7c148-c707-0410-ad76-f59c66615308

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@
 # form the root of the application path.
 
 set :application, "4ever"
-set :repository, "https://store.sabretechllc.com/public/#{application}/trunk"
+set :repository, "http://store.sabretechllc.com/public/#{application}/trunk"
 
 # =============================================================================
 # ROLES
