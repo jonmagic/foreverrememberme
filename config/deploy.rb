37ca665e7b7df011f21a67a2ded262659ca90dd3
Jonathan Hoyt <jonmagic@Sarah.local>
2009-04-28 13:24:26 -0400

    getting ready for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,123 +5,45 @@
 # =============================================================================
 # REQUIRED VARIABLES
 # =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
 
 set :application, "4ever"
-set :repository, "http://store.sabretechllc.com/public/#{application}/trunk"
+
+set :repository, "git@github.com:jonmagic/foreverrememberme.git"
+set :scm_user, 'jonmagic'
+set :scm_passphrase, "316w4ldr0n"
+set :scm, :git
+set :deploy_via, :remote_cache
+set :branch, "master"
 
 # =============================================================================
 # ROLES
 # =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
 
-role :web, "www.foreverrememberme.com"
-role :app, "www.foreverrememberme.com"
-role :db,  "www.foreverrememberme.com"
-
+set :user, "forever"
+set :domain, "www.foreverrememberme.com"
+server domain, :app, :web
+role :db, domain, :primary => true
 
 # =============================================================================
 # OPTIONAL VARIABLES
 # =============================================================================
-set :deploy_to, "/home/forever/apps" # defaults to "/u/apps/#{application}"
-set :user, "forever"            # defaults to the currently logged in user
-# set :scm, :darcs               # defaults to :subversion
-# set :svn, "/path/to/svn"       # defaults to searching the PATH
-# set :darcs, "/path/to/darcs"   # defaults to searching the PATH
-# set :cvs, "/path/to/cvs"       # defaults to searching the PATH
-# set :gateway, "gate.host.com"  # default to no gateway
+set :user_path, "/home/forever"
+set :deploy_to, "#{user_path}/apps"
+set :release_path, "#{deploy_to}/current"
 
 # =============================================================================
 # TASKS
 # =============================================================================
-# Define tasks that run on all (or only some) of the machines. You can specify
-# a role (or set of roles) that each task should be executed on. You can also
-# narrow the set of servers to a subset of a role by specifying options, which
-# must match the options given for the servers to select (like :primary => true)
 
-desc <<DESC
-An imaginary backup task. (Execute the 'show_tasks' task to display all
-available tasks.)
-DESC
-task :backup, :roles => :db, :only => { :primary => true } do
-  # the on_rollback handler is only executed if this task is executed within
-  # a transaction (see below), AND it or a subsequent task fails.
-  on_rollback { delete "/tmp/dump.sql" }
+after "deploy:update", "forever:update_configs"
 
-  run "mysqldump -u theuser -p thedatabase > /tmp/dump.sql" do |ch, stream, out|
-    ch.send_data "thepassword\n" if out =~ /^Enter password:/
+namespace :forever do
+  desc "This task sets up all my particular symlinks"
+  task :update_configs do
+    run "cp #{user_path}/pro_config/production_v1/config/database.yml #{release_path}/config/"
+    run "ln -s #{user_path}/files/picture #{release_path}/public/picture"
+    run "ln -s #{user_path}/files/music #{release_path}/public/music"
+    run "chmod 755 #{release_path}/public"
+    run "chmod 755 #{release_path}/public/*"
   end
 end
-
-# Tasks may take advantage of several different helper methods to interact
-# with the remote server(s). These are:
-#
-# * run(command, options={}, &block): execute the given command on all servers
-#   associated with the current task, in parallel. The block, if given, should
-#   accept three parameters: the communication channel, a symbol identifying the
-#   type of stream (:err or :out), and the data. The block is invoked for all
-#   output from the command, allowing you to inspect output and act
-#   accordingly.
-# * sudo(command, options={}, &block): same as run, but it executes the command
-#   via sudo.
-# * delete(path, options={}): deletes the given file or directory from all
-#   associated servers. If :recursive => true is given in the options, the
-#   delete uses "rm -rf" instead of "rm -f".
-# * put(buffer, path, options={}): creates or overwrites a file at "path" on
-#   all associated servers, populating it with the contents of "buffer". You
-#   can specify :mode as an integer value, which will be used to set the mode
-#   on the file.
-# * render(template, options={}) or render(options={}): renders the given
-#   template and returns a string. Alternatively, if the :template key is given,
-#   it will be treated as the contents of the template to render. Any other keys
-#   are treated as local variables, which are made available to the (ERb)
-#   template.
-
-desc "Demonstrates the various helper methods available to recipes."
-task :helper_demo do
-  # "setup" is a standard task which sets up the directory structure on the
-  # remote servers. It is a good idea to run the "setup" task at least once
-  # at the beginning of your app's lifetime (it is non-destructive).
-  setup
-
-  buffer = render("maintenance.rhtml", :deadline => ENV['UNTIL'])
-  put buffer, "#{shared_path}/system/maintenance.html", :mode => 0644
-#  sudo "killall -USR1 dispatch.fcgi"
-  run "#{release_path}/script/spin"
-  delete "#{shared_path}/system/maintenance.html"
-end
-
-# You can use "transaction" to indicate that if any of the tasks within it fail,
-# all should be rolled back (for each task that specifies an on_rollback
-# handler).
-
-desc "A task demonstrating the use of transactions."
-task :long_deploy do
-  transaction do
-    update_code
-    symlink
-    migrate
-  end
-end
-
-desc "This task sets up all my particular symlinks"
-task :after_symlink do
-  run "rm -f #{release_path}/config/database.yml"
-  run "cp ~/pro_config/production_v1/config/database.yml #{release_path}/config/"
-  run "ln -s ~/files/picture #{release_path}/public/picture"
-  run "ln -s ~/files/music #{release_path}/public/music"
-  run "chmod 755 ~/apps/current/public"
-  run "chmod 755 ~/apps/current/public/*"
-end
-
-desc "Don't restart the web server"
-  task :restart, :roles => :app do
-    run ""
-  end
